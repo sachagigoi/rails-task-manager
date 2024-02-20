391c29b2ec932dd4b562e2c6d8f2eb145125cc9d
Sacha Gigoi <gigoi.sacha@gmail.com>
2024-02-20 14:46:06 +0100

    pas de javascript dans manifest

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  delete "tasks/:id", to: "tasks#destroy"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: :newtask
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edittask
+  get "tasks/:id", to: "tasks#show", as: :task
+  patch "tasks/:id", to: "tasks#update"
+
 end
